f91df503dd78de013a4b8662cdefce971192f8b3
RaiDiwakar23 <diwakar.r@qspiders.in>
2024-08-30 17:09:45 +0530

    directives completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatabindingComponent } from './components/databinding/databinding.component';
+import { StructuraldirectiveComponent } from './components/structuraldirective/structuraldirective.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatabindingComponent],
+  imports: [RouterOutlet, StructuraldirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/structuraldirective/structuraldirective.component.ts
+++ src/app/components/structuraldirective/structuraldirective.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structuraldirective',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './structuraldirective.component.html',
   styleUrl: './structuraldirective.component.css',
 })
@@ -12,6 +13,51 @@ export class StructuraldirectiveComponent {
   showBlock: boolean = false;
   state: string = '';
   dayNo: string = '1';
+
+  isred: boolean = true;
+
+  studentData: any = [
+    {
+      name: 'Virat Kohli',
+      age: 30,
+      isgraduate: true,
+      imgsource:
+        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww',
+    },
+    {
+      name: 'Rohit Sharma',
+      age: 33,
+      isgraduate: false,
+      imgsource:
+        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww',
+    },
+    {
+      name: 'Suresh Raina',
+      age: 35,
+      isgraduate: true,
+      imgsource:
+        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww',
+    },
+    {
+      name: 'Jasprit Bumrah',
+      age: 35,
+      isgraduate: true,
+      imgsource:
+        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww',
+    },
+  ];
+
+  cityList: string[] = [
+    'Mumbai',
+    'Bangalore',
+    'Hyderabad',
+    'Pune',
+    'Kolkata',
+    'Shillong',
+    'Patna',
+  ];
+
+  countries: string[] = ['India', 'Nepal', 'Bhutan', 'USA', 'Germany'];
 
   showElement() {
     this.showBlock = true;
@@ -29,5 +75,9 @@ export class StructuraldirectiveComponent {
     // }
 
     this.showBlock = !this.showBlock;
+  }
+
+  changeBg() {
+    this.isred = !this.isred;
   }
 }
